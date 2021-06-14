b269a5e5a15f47001c72f6a1843bb0a5dd5048d7
Levon Soghomonyan <lsoghomo@c1r11s3.42yerevan.am>
2021-06-14 18:48:04 +0400

    ok

---
+++ src/common/actions/action3.c
@@ -0,0 +1,22 @@
+#include <common/common.h>
+
+void	rotate_b_a(t_mem *mem)
+{
+	put_top_to_bottom(&(mem->a));
+	put_top_to_bottom(&(mem->b));
+	if (mem->print)
+		ft_putstr("rr");
+}
+
+void	ft_puterr(char *str)
+{
+	int		i;
+	char	n;
+
+	i = 0;
+	while (str[i])
+		i++;
+	n = '\n';
+	write(2, str, i);
+	write(2, &n, 1);
+}
